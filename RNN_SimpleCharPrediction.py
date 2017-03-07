a9f371a4bba8ec652d4a2c9368ff907ef78e38a3
Christopher Frazier <ChristopherF@utexas.edu>
2017-03-07 17:35:37 -0600

    Adding data and uploading old attempt

--- RNN_SimpleCharPrediction.py
+++ RNN_SimpleCharPrediction.py
@@ -2,3 +2,24 @@
 Apply a simple RNN to predict various lengths of character sequences
 This is just meant to be a fun little exercise
 '''
+# Base python includes
+from __future__ import print_function
+import numpy as np
+import itertools
+np.random.seed(327)  # for reproducibility
+
+# Keras includes
+from Keras import sequence
+
+
+# Import data from data_dir, format to X_train, y_trian, etc.
+data_dir = 'data/big.txt'
+with open('data/big.txt') as file:
+    raw_data = np.array(
+        list(itertools.chain.from_iterable(
+            [list(line.rstrip()) for line in f]
+        ))
+    )
+
+
+# Create and compile Keras net
