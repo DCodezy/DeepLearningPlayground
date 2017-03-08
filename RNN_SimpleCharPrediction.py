effdd6232f713fbdb2eb33313563b8443a2a1bfe
Christopher Frazier <ChristopherF@utexas.edu>
2017-03-07 18:54:21 -0600

    On server fixes

--- RNN_SimpleCharPrediction.py
+++ RNN_SimpleCharPrediction.py
@@ -7,8 +7,6 @@ import numpy as np
 import itertools
 np.random.seed(327)  # for reproducibility
 
-from Keras import sequence
-
 # Import data from data_dir, format to X_train, y_trian, etc.
 remove_chars = '\t=@[]<>^_|~%*'
 window_size = 25
@@ -31,14 +29,13 @@ int_data = [char2int_dic[i] for i in raw_data]
 # Create sequences by moving size of $window_size
 seq_in = []
 exp_out = []
-for i in range(0, len(result) - window_size):
-    seq_in.append(int_data[i:(i + window_size))
+for i in range(0, len(int_data) - window_size):
+    seq_in.append(int_data[i:(i + window_size)])
     exp_out.append(int_data[i + window_size])
 
-print("Sequence in:")
-print(seq_in)
-print("exp_out:")
-print(exp_out)
+print("Sequence in len: " + str(len(seq_in)))
+print(seq_in[0])
+print("exp_out len: " + str(len(exp_out)))
 # Vectorize sequences and from to 3d array (n_samples, time_step, input_dim)
 # Note: input_dim is to represent the vectorized form of input
 
