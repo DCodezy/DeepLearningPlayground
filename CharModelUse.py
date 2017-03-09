c8fb33ee6c69d9bd55279baa3e0f404e8d48c311
DCodezy <christopherf@utexas.edu>
2017-03-09 16:47:04 -0600

    Init gen model

--- CharModelUse.py
+++ CharModelUse.py
@@ -53,17 +53,25 @@ model_seed_input = np.zeros((1, len(SENTENCE_SEED), 53), dtype=np.bool)
 
 remove_chars = '\t=@[]<>^_|~%*'
 mod_sentence_seed = list(SENTENCE_SEED.lower())
-sentence_ints = [char2int_dic[c] for c in mod_sentence_seed 
+sentence_ints = [char2int_dic[c] for c in mod_sentence_seed
 						if c not in remove_chars]
 for (i, c) in enumerate(sentence_ints):
     model_seed_input[0, i, c] = 1
 
-pred = loaded_model.predict(model_seed_input, verbose=0)[0]
-print("Printing pred:")
-print(pred)
-print("Done")
-print(int2char_dic[np.argmax(pred)])
-'''
+output_sentence = SENTENCE_SEED
+
 for i in END_SENTECE_LENGTH:
-    pred = loaded_model.predict(model_seed_input, verbose=0)
-'''
+    pred_logprob = loaded_model.predict(model_seed_input, verbose=0)[0]
+    pred_int = np.argmax(pred_logprob)
+
+    output_sentence += int2char_dic[pred_int]
+
+    # Shift model input to include new character
+    model_seed_input = np.roll(model_seed_input, shift=-1, axis=1)
+    model_seed_input[0, SEED_LENGTH - 1] = np.zeros((53), dtype=np.bool)
+    model_seed_input[0, SEED_LENGTH - 1, pred_int] = 1
+
+print("Input: ")
+print(SENTENCE_SEED)
+print("Output: ")
+print(output_sentence)
